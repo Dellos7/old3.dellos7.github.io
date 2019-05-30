3aaba115de2f17df22b59df0c2dd83b82214e6c2
David López Castellote <david.lopez.castellote@gmail.com>
2019-05-30 18:26:10 +0200

    fixed html height and add icons

--- sw.js
+++ sw.js
@@ -27,17 +27,17 @@ self.addEventListener('message', (event) => {
 self.__precacheManifest = [
   {
     "url": "index-org.html",
-    "revision": "5c94faa49ddc440f8872cb9746792750"
+    "revision": "6df4c431f88e10021ac95e5c368b3ac7"
   },
   {
     "url": "index.html",
-    "revision": "5c94faa49ddc440f8872cb9746792750"
+    "revision": "6df4c431f88e10021ac95e5c368b3ac7"
+  },
+  {
+    "url": "build/p-0xz0rwso.entry.js"
   },
   {
     "url": "build/p-229ebf7a.js"
-  },
-  {
-    "url": "build/p-2nyoqigb.entry.js"
   },
   {
     "url": "build/p-4orpccr3.entry.js"
@@ -46,10 +46,16 @@ self.__precacheManifest = [
     "url": "build/p-549b16dd.js"
   },
   {
+    "url": "build/p-5rd6dne9.css"
+  },
+  {
     "url": "build/p-7bf95aa7.js"
   },
   {
-    "url": "build/p-8kds6mou.entry.js"
+    "url": "build/p-7ibfshpu.entry.js"
+  },
+  {
+    "url": "build/p-7zsfxq9a.entry.js"
   },
   {
     "url": "build/p-9ldesl56.entry.js"
@@ -58,16 +64,13 @@ self.__precacheManifest = [
     "url": "build/p-a0c82e31.js"
   },
   {
+    "url": "build/p-d2gug66v.entry.js"
+  },
+  {
     "url": "build/p-ddbvt1at.entry.js"
   },
   {
     "url": "build/p-e74c9e26.js"
-  },
-  {
-    "url": "build/p-eojrntio.css"
-  },
-  {
-    "url": "build/p-erwkqjnm.entry.js"
   },
   {
     "url": "build/p-facb6cf1.js"
@@ -76,7 +79,7 @@ self.__precacheManifest = [
     "url": "build/p-i9vgcqe5.entry.js"
   },
   {
-    "url": "build/p-jcgecljh.entry.js"
+    "url": "build/p-je6qax7m.entry.js"
   },
   {
     "url": "build/p-lqcjuu7w.entry.js"
@@ -85,13 +88,10 @@ self.__precacheManifest = [
     "url": "build/p-nkqm4h2h.entry.js"
   },
   {
-    "url": "build/p-rdkivyer.entry.js"
+    "url": "build/p-sxpeiasr.entry.js"
   },
   {
     "url": "build/p-tmmcezdc.entry.js"
-  },
-  {
-    "url": "build/p-ttnvdmoz.entry.js"
   },
   {
     "url": "build/p-wjxttlkh.entry.js"
@@ -106,7 +106,7 @@ self.__precacheManifest = [
   },
   {
     "url": "manifest.json",
-    "revision": "0c129721ede7cd25304ebdd238d774ad"
+    "revision": "23ede5c391f4fbe2b55905cdf85c382a"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
